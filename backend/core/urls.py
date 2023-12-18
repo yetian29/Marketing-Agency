9aec4fe11d7e091b712c1d39cabdcb7dfbf5c1f7
yetian29 <dathien000@gmail.com>
2023-12-18 09:23:50 +0700

    Add

--- backend/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from django.conf import settings
-from djagno.conf.urls.static import static
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
